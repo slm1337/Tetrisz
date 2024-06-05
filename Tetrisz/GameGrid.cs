405d376c855dc6fef69c8f01770048761ad202a2
Kayurov Dmytro <e1337ajke@gmail.com>
2024-06-05 20:40:35 +0300

    Добавьте файлы проекта.

---
+++ Tetrisz/GameGrid.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+using System.Security.Policy;
+
+namespace Tetrisz
+{
+    public class GameGrid
+    {
+        private readonly int[,] grid;
+        public int Rows { get; }
+        public int Columns { get; }
+
+        public int this[int r, int c]
+        {
+            get => grid[r, c];
+            set => grid[r, c] = value;
+        }
+
+        public GameGrid(int rows, int colums) {
+            Rows = rows;
+            Columns = colums;
+            grid = new int[Rows, Columns];
+        }
+
+        public bool IsInSIDE(int r, int c) { 
+            return r >= 0 && r < Rows && c >= 0 && c < Columns;
+        }
+
+        public
+    }
+}
